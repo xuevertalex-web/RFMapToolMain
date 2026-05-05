b4805b930e79a475f2972479073c2f62e9e04668
Azrael-ueb <82717292+Azrael-ueb@users.noreply.github.com>
2026-05-05 19:22:10 +0300

    refactor: cache local line count alias in matching lookup

--- Core/Agent.cs
+++ Core/Agent.cs
@@ -1191,7 +1191,8 @@ Use only the registered tools exactly as listed in the prompt. The only valid to
             var localLines = lines;
             var notFound = NotFound;
             var needleComparison = NeedleComparison;
-            for (var i = 0; i < lineCount; i++)
+            var localLineCount = lineCount;
+            for (var i = 0; i < localLineCount; i++)
             {
                 if (localLines[i].IndexOf(localNeedle, needleComparison) >= 0)
                 {
